6d53543cc1333baf49d0b66d86d93d0ade20e7d9
Kimberley Clark <kimjanehunter@hotmail.com>
2019-02-15 14:43:12 +0000

    MVP

---
+++ js/app.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+
+  //find the form add eventListner onto
+
+  const form = document.querySelector('#new-form')
+    form.addEventListener('submit', handleFormSubmit)
+
+
+
+    const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
+
+})
+
+const handleFormSubmit = function(event){
+  event.preventDefault()
+
+  const pokemonListItem = createPokemonListItem(event.target);
+  const pokemonLibrary = document.querySelector('#pokemon-library');
+  pokemonLibrary.appendChild(pokemonListItem)
+
+  event.target.reset();
+}
+
+
+const createPokemonListItem = function (form) {
+
+  const pokemonListItem = document.createElement('li');
+  pokemonListItem.classList.add('pokemon-list-item');
+
+
+  const nameItem = document.createElement('h2');
+  nameItem.textContent = form.name.value;
+  pokemonListItem.appendChild(nameItem);
+
+  const skill = document.createElement('p');
+  skill.textContent = form.skill.value;
+  pokemonListItem.appendChild(skill);
+
+  const combat_points = document.createElement('p');
+  combat_points.textContent = form.combat_points.value;
+  pokemonListItem.appendChild(combat_points);
+
+  return pokemonListItem;
+}
+
+const handleDeleteAllClick = function (event) {
+  const pokemonLibrary = document.querySelector('#pokemon-library');
+  pokemonLibrary.innerHTML = '';
+}
